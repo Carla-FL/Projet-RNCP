ff6b43dbd790056c7c2817c042190de51375b344
Carla-FL <carla.flpro@gmail.com>
2025-09-16 07:26:15 +0200

    comment prefect

--- src/Pipeline1/etl.py
+++ src/Pipeline1/etl.py
@@ -6,18 +6,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.utils.extraction import Extraction
 from src.utils.transformation import main_transformation
 from src.utils.load import Load
-from prefect import flow, task
-from prefect.logging import get_run_logger
+# from prefect import flow, task
+# from prefect.logging import get_run_logger
 import pandas as pd
 
 
 
-@flow(name='main_etl_flow', description="Pipeline ETL principal pour l'analyse des vidéos YouTube")
+# @flow(name='main_etl_flow', description="Pipeline ETL principal pour l'analyse des vidéos YouTube")
 def main_etl(url:str,last_id=None, with_channel_id:bool=False, maj=False):
     """
     
     """
-    logger = get_run_logger()
+    # logger = get_run_logger()
     try:
         
     
@@ -27,21 +27,21 @@ def main_etl(url:str,last_id=None, with_channel_id:bool=False, maj=False):
         # Étape 1 : Extraction des données
         extraction = Extraction(video_url=input_video_url)
         data, video_id, channel_id = extraction.main_extraction()
-        logger.info(f"fin de l'extraction des {data.shape[0]} données")
+        # logger.info(f"fin de l'extraction des {data.shape[0]} données")
         # Étape 2 : Transformation des données
         transformed_data = main_transformation(data)
-        logger.info("Transformation des données terminée.")
+        # logger.info("Transformation des données terminée.")
 
         # Étape 3 : Chargement des données
         loader = Load(maj)
         loader.load(transformed_data, video_id, channel_id)
-        logger.info("Chargement des données terminé.")
-        logger.info("Pipeline ETL terminé avec succès.")
+        # logger.info("Chargement des données terminé.")
+        # logger.info("Pipeline ETL terminé avec succès.")
         if with_channel_id:
             return channel_id
     except Exception as e:
-        logger.info(f"Une erreur s'est produite dans le pipeline ETL : {e}")
-        #raise ValueError(f"Erreur dans le pipeline ETL : {e}")
+        # logger.info(f"Une erreur s'est produite dans le pipeline ETL : {e}")
+        raise ValueError(f"Erreur dans le pipeline ETL : {e}")
 
 """ ________________________________________________________________  Main  ________________________________________________________________"""
 # if __name__ == "__main__":
